a285739c8ad69923ad0e82c91b481a0d63ce855c
Trass3r <un@known.com>
2010-12-15 16:08:21 +0100

    colored bumps pause function no more cube

--- main.d
+++ main.d
@@ -14,6 +14,8 @@ import dsfml.graphics.all;
 import derelict.opengl.gl;
 import derelict.opengl.glu;
 
+import colormaps;
+
 enum int Nx = 10; // # Neuronen pro Zeile
 enum int Ny = cast(int) (sqrt(3)/2*Nx + 0.5); // rounded, should be 9 for Nx=10
 enum N = Nx*Ny; //! # neurons
@@ -51,7 +53,7 @@ void reshape (int w, int h)
 }
 
 //__gshared float xpos = 851.078f, ypos = 351.594f, zpos = 281.033f, xrot = 758f, yrot = 238f, angle=0.0f, cScale=1.0f;
-__gshared float xpos = 0f, ypos = 0f, zpos = 0f, xrot = 1f, yrot = 1f, angle=0.0f, cScale=0.01f;
+__gshared float xpos = 8.052496f, ypos = 3.747924f, zpos = -3.023191f, xrot = 753f, yrot = 932f, angle=0.0f, cScale=0.02f;
 __gshared int lastx = 0, lasty = 0;
 
 void camera ()
@@ -81,6 +83,9 @@ static void mouseMovement(int x, int y)
 
 void main()
 {
+	bool updateValues = true;
+	float max = 0;
+	
 	RenderWindow window = new RenderWindow(VideoMode(1024, 768), "CANN", Style.Default, ContextSettings(24,8,0,2,0));
 	
 	DerelictGL.load();
@@ -123,6 +128,11 @@ void main()
 				case EventType.Resized:
 					reshape(window.width, window.height);
 					break;
+				case EventType.KeyPressed:
+					if (evt.Key.Code == KeyCode.U)
+						updateValues = !updateValues;
+					else if (evt.Key.Code == KeyCode.I)
+						writefln("%f %f %f %f %f", xpos, ypos, zpos, xrot, yrot);
 				default:
 			}
 		}
@@ -134,14 +144,14 @@ void main()
 		{
 			xpos += sin(yrotrad) * cScale;
 			zpos -= cos(yrotrad) * cScale;
-			ypos -= sin(xrotrad);
+			ypos -= sin(xrotrad) * cScale;
 		}
 		
 		if (window.input.isKeyDown(KeyCode.S))
 		{
 			xpos -= sin(yrotrad) * cScale;
 			zpos += cos(yrotrad) * cScale;
-			ypos += sin(xrotrad);
+			ypos += sin(xrotrad) * cScale;
 		}
 
 		if (window.input.isKeyDown(KeyCode.D))
@@ -158,42 +168,45 @@ void main()
 
 		mouseMovement(window.input.mouseX, window.input.mouseY);
 
-		// one time step
-		float sumA = 0;
-		foreach(e; A)
-			sumA += e;
-	
-		for(int i=0; i<N; i++)
+		if (updateValues)
 		{
-			auto veci = idx2vec(i);
-			int ix = i % Nx; // Spaltennummer
-			int iy = i / Nx; // Zeilennummer
-
-			
-			// Berechne B
-			float tmp = 0;
-			for(int j=0; j<N; j++)
+			// one time step
+			float sumA = 0;
+			foreach(e; A)
+				sumA += e;
+		
+			for(int i=0; i<N; i++)
 			{
-				auto vecj = idx2vec(j);
-				int jx = j % Nx;
-				int jy = j / Nx;
-
-				float d = distTriSqr( (jx - ix) / cast(float)Nx  + gain * (cos(bias)*vx -sin(bias) + vy), sqr32 * (jy - iy) / cast(float) Ny  + gain * (sin(bias)*vx + cos(bias)*vy));
-				tmp += A[j] * ( I * exp( -(d)/(sigma^^2) ) - T );
+				auto veci = idx2vec(i);
+				int ix = i % Nx; // Spaltennummer
+				int iy = i / Nx; // Zeilennummer
+	
+				
+				// Berechne B
+				float tmp = 0;
+				for(int j=0; j<N; j++)
+				{
+					auto vecj = idx2vec(j);
+					int jx = j % Nx;
+					int jy = j / Nx;
+	
+					float d = distTriSqr( (jx - ix) / cast(float)Nx  + gain * (cos(bias)*vx -sin(bias) + vy), sqr32 * (jy - iy) / cast(float) Ny  + gain * (sin(bias)*vx + cos(bias)*vy));
+					tmp += A[j] * ( I * exp( -(d)/(sigma^^2) ) - T );
+				}
+				
+				// Berechne A
+				Aneu[i] = (1-tau)*tmp + tau*(tmp/(sumA));
+				if (Aneu[i] < 0)
+					Aneu[i] = 0;
 			}
 			
-			// Berechne A
-			Aneu[i] = (1-tau)*tmp + tau*(tmp/(sumA));
-			if (Aneu[i] < 0)
-				Aneu[i] = 0;
+			A = Aneu; // copy
+			
+			max = 0;
+			foreach(e; A)
+				if (e > max)
+					max = e;
 		}
-		
-		A = Aneu; // copy
-		
-		float max = 0;
-		foreach(e; A)
-			if (e > max)
-				max = e;
 		
 		glClearColor(0.0, 0.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -205,52 +218,24 @@ void main()
 			for (int x=0; x<Nx-1; x++)
 				for (int y=0; y<Ny-1; y++)
 				{
-					glColor3f(1f, 1f, 1f);
-					glVertex3f(x,   A[y*Nx + x], y);
-					glVertex3f(x,   A[(y+1)*Nx + x], y+1);
-					glVertex3f(x+1, A[y*Nx + x+1], y);
-					glVertex3f(x+1, A[(y+1)*Nx + x+1], y+1);
+					Color c = jet[cast(size_t) (A[y*Nx + x]/max * 1023)];
+					glColor3f(c.r, c.g, c.b);
+					glVertex3f(x,   A[y*Nx + x]/max, y);
+
+					c = jet[cast(size_t) (A[(y+1)*Nx + x]/max * 1023)];
+					glColor3f(c.r, c.g, c.b);
+					glVertex3f(x,   A[(y+1)*Nx + x]/max, y+1);
+
+					c = jet[cast(size_t) (A[y*Nx + x+1]/max * 1023)];
+					glColor3f(c.r, c.g, c.b);
+					glVertex3f(x+1, A[y*Nx + x+1]/max, y);
+
+					c = jet[cast(size_t) (A[(y+1)*Nx + x+1]/max * 1023)];
+					glColor3f(c.r, c.g, c.b);
+					glVertex3f(x+1, A[(y+1)*Nx + x+1]/max, y+1);
 				}
 		glEnd();
 
-
-		glBegin(GL_QUADS);
-			glColor3f(1, 0,   0  );   // Ab jetzt werden alle gezeichneten Punkte rot
-		      glVertex3f( 1,  1, -1);
-		      glVertex3f( 1, -1, -1);
-		      glVertex3f(-1, -1, -1);
-		      glVertex3f(-1,  1, -1);
-
-		    glColor3f(0, 1,   0  );   // Ab jetzt werden alle gezeichneten Punkte grün
-		      glVertex3f( 1,  1,  1);
-		      glVertex3f(-1,  1,  1);
-		      glVertex3f(-1, -1,  1);
-		      glVertex3f( 1, -1,  1);
-
-		    glColor3f(0, 0,   1  );
-		      glVertex3f( 1,  1, -1);
-		      glVertex3f( 1,  1,  1);
-		      glVertex3f( 1, -1,  1);
-		      glVertex3f( 1, -1, -1);
-
-		    glColor3f(1, 1,   0  );
-		      glVertex3f( 1, -1, -1);
-		      glVertex3f( 1, -1,  1);
-		      glVertex3f(-1, -1,  1);
-		      glVertex3f(-1, -1, -1);
-
-		    glColor3f(0, 0.5, 1  );
-		      glVertex3f(-1, -1, -1);
-		      glVertex3f(-1, -1,  1);
-		      glVertex3f(-1,  1,  1);
-		      glVertex3f(-1,  1, -1);
-
-		    glColor3f(1, 0.1, 0.8);
-		      glVertex3f( 1,  1,  1);
-		      glVertex3f( 1,  1, -1);
-		      glVertex3f(-1,  1, -1);
-		      glVertex3f(-1,  1,  1);
-		glEnd();
 		window.display();
 	}
 	writeln("done");
